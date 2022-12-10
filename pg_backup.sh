1cb60ee16b2f8e4d5001d4d3afbdf8d45e0a1126
Vikram-Karamala <66124306+Vikram-Karamala@users.noreply.github.com>
2022-12-10 16:48:58 +0530

    Update pg_backup.sh

--- pg_backup.sh
+++ pg_backup.sh
@@ -107,7 +107,7 @@ fi;
 		
 		echo -e " "
 		echo -e "Sync current backup to azure blob"
-		/root/pg_backup/azcopy sync "$FINAL_BACKUP_DIR$DATABASE/current" "$BLOB_URL/$CURRENT_DATE/$DATABASE/$BLOB_SAS"
+		/root/pg_backup/azcopy copy "$FINAL_BACKUP_DIR$DATABASE/current" "$BLOB_URL/$CURRENT_DATE/$DATABASE/$BLOB_SAS"
  
 		## Sync backup log & delete backup log to azure
 		echo -e " "
